6b3056cb539a99373d0bac835d3fb09f6f25cb7b
Ayush Patel <ayush@atyantik.com>
2025-01-21 19:19:12 +0530

    Usefull build configuration
    which help to automatically assign buid to specific release, enviorment, name of release & etc...

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import { environment, release } from "./src/main";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "learn-sentry-eh",
-    project: "release-deployment"
+    project: "release-deployment",
+    release: {
+      name: "release-deom@1.0.0",
+      deploy: {
+        // env: environment,
+        env: "staging",
+      },
+      create: true,
+      finalize: true,
+    },
+    // authToken: process.env.SENTRY_AUTH_TOKEN
+    authToken: "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",
   })],
 
   build: {
